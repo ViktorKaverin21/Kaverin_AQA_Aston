ce923c009747a9c0f06eccb14b2cc813d6e7051a
ViktorKaverin21 <131533133+ViktorKaverin21@users.noreply.github.com>
2024-02-05 17:54:21 +0300

    Update AppData.java

--- Lesson_6/src/main/java/org/example/AppData.java
+++ Lesson_6/src/main/java/org/example/AppData.java
@@ -18,14 +18,14 @@ public class AppData {
     }
 
     public void save(String filePath) throws IOException {
-<<<<<<< HEAD
+
         try (CSVWriter writer = new CSVWriter(new FileWriter(filePath), ';',
                 CSVWriter.DEFAULT_QUOTE_CHARACTER,
                 CSVWriter.DEFAULT_ESCAPE_CHARACTER,
                 CSVWriter.DEFAULT_LINE_END)) {
-=======
+
         try (CSVWriter writer = new CSVWriter(new FileWriter(filePath))) {
->>>>>>> a2a0b04fa99a67e0f825773abe5d0b3827429830
+
             writer.writeNext(header);
 
             for (int[] row : data) {
@@ -38,11 +38,10 @@ public class AppData {
         }
     }
 
-<<<<<<< HEAD
     public void load(String filePath) throws IOException, CsvValidationException {
-=======
+
     public void load(String filePath) throws IOException {
->>>>>>> a2a0b04fa99a67e0f825773abe5d0b3827429830
+
         try (CSVReader reader = new CSVReader(new FileReader(filePath))) {
             try {
                 header = reader.readNext();
@@ -51,13 +50,13 @@ public class AppData {
             }
 
             String[] nextLine;
-<<<<<<< HEAD
+
             while ((nextLine = reader.readNext()) != null) {
                 int[] row = new int[nextLine.length];
                 for (int i = 0; i < nextLine.length; i++) {
                     String[] rowData = nextLine[i].split(";");
                     row[i] = Integer.parseInt(rowData[0].replaceAll("\"", ""));
-=======
+
             while (true) {
                 try {
                     if (!((nextLine = reader.readNext()) != null))
@@ -68,13 +67,11 @@ public class AppData {
                 int[] row = new int[nextLine.length];
                 for (int i = 0; i < nextLine.length; i++) {
                     row[i] = Integer.parseInt(nextLine[i]);
->>>>>>> a2a0b04fa99a67e0f825773abe5d0b3827429830
+
                 }
             }
         }
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> a2a0b04fa99a67e0f825773abe5d0b3827429830
+
+
