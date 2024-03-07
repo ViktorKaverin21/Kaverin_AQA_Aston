b42605d41615c53d4d78b5842956638fa146dbb8
ViktorKaverin21 <131533133+ViktorKaverin21@users.noreply.github.com>
2024-03-07 10:04:27 +0300

    Delete Lesson_15_Wb directory

--- Lesson_15_Wb/src/main/java/org/example/config/locators/HeaderLocators.java
+++
@@ -1,9 +0,0 @@
-package org.example.config.locators;
-
-import org.openqa.selenium.By;
-
-public class HeaderLocators {
-    public static By basketButtonLocator = (By.cssSelector("[data-wba-header-name='Cart']"));
-    public static By basketBubbleLocator = (By.cssSelector("[data-wba-header-name='Cart'] .navbar-pc__notify"));
-
-}
